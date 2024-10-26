153447d696789f59997c97c6b267016d4d3ce17c
Paris <32800711+MacCaptKim@users.noreply.github.com>
2024-10-26 06:38:49 -0700

    Update deployments.ts

--- src/slashCommands/deployments.ts
+++ src/slashCommands/deployments.ts
@@ -6,9 +6,8 @@ import Backups from "../tables/Backups.js";
 import Deployment from "../tables/Deployment.js";
 import Config from "../config.js";
 
-//what a madlad
-
-class CustomEmbedBuilder extends EmbedBuilder {
+// "Hacked" the embed builder to bypass name empty string restrictions
+class HackedEmbedBuilder extends EmbedBuilder {
     addFields(...fields) {
         // Map over fields and remove the validation on the `name` field
         fields = fields.map(field => ({
@@ -28,6 +27,7 @@ interface assignedDeployment {
     id: number;
     title: string;
     time: number;
+    channel: string;
     assignment: string;
 }
 
@@ -43,6 +43,7 @@ const getUserDeployments = async (userID: string): Promise<assignedDeployment[]>
                 id: deployment[0].id,
                 title: deployment[0].title,
                 time: deployment[0].startTime,
+                channel: deployment[0].channel,
                 assignment: signup.userId === deployment[0].user ? 'leader' : 'primary'
             });
         }
@@ -56,6 +57,7 @@ const getUserDeployments = async (userID: string): Promise<assignedDeployment[]>
                 id: deployment[0].id,
                 title: deployment[0].title,
                 time: deployment[0].startTime,
+                channel: deployment[0].channel,
                 assignment: 'backup'
             });
         }
@@ -79,7 +81,7 @@ const buildDeploymentsEmbed = async (user: string): Promise<EmbedBuilder> => {
     const deployments = await getUserDeployments(user);
 
     // Create the embed
-    const embed = new CustomEmbedBuilder()
+    const embed = new HackedEmbedBuilder()
         .setTitle(deployments.length == 0 ? Config.embeds.deploymentsCommand.title.noDeployments : Config.embeds.deploymentsCommand.title.default)
         .setColor(0xb60000)
         .setTimestamp(DateTime.now().toJSDate());
@@ -88,12 +90,12 @@ const buildDeploymentsEmbed = async (user: string): Promise<EmbedBuilder> => {
         const title = deployment.title;
         const time = deployment.time;
         const assignment = deployment.assignment;
-       // const link = `https://discord.com/channels/${Config.guildId}/1299122351291629599/${deployment.id}`;
+        const link = `https://discord.com/channels/${Config.guildId}/${deployment.channel}/${deployment.id}`;
 
         // Add field to the embed
         embed.addFields({
             name: '',
-            value: `${getStatusEmoji(assignment)} ᲼**Operation:** ${title} | **Time:** <t:${Math.round(time / 1000)}:t> | **Assignment:** ${assignment.charAt(0).toUpperCase() + assignment.slice(1)} | [🔗]`
+            value: `${getStatusEmoji(assignment)} ᲼**Operation:** ${title} | **Time:** <t:${Math.round(time / 1000)}:t> | **Assignment:** ${assignment.charAt(0).toUpperCase() + assignment.slice(1)} | [🔗](${link})`
         });
     });
 
