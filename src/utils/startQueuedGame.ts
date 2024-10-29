a5cf3d90cd15f581344d0d8f6259ad903c732b44
srmullaney <srmullaney@gmail.com>
2024-10-29 00:15:03 -0400

    Lol forgot the __dirname in my path.join

--- src/utils/startQueuedGame.ts
+++ src/utils/startQueuedGame.ts
@@ -32,7 +32,7 @@ export const startQueuedGame = async (deploymentTime: number) => {
     }
 
     // Read the deployment interval from the file
-    const deploymentIntervalPath = path.join('..', '..', 'deploymentTime.txt');
+    const deploymentIntervalPath = path.join(__dirname, '..', '..', 'deploymentTime.txt');
     const deploymentIntervalMs = parseInt(await fs.readFile(deploymentIntervalPath, 'utf-8'), 10);
     console.log(`Deployment interval read from file: ${deploymentIntervalMs} ms`);
 
