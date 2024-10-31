103b44e34679fc30f99f02808063d5ae33726bbd
srmullaney <srmullaney@gmail.com>
2024-10-31 04:27:49 -0400

    trying something

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -145,7 +145,7 @@ export default {
 					} 
 				});
 
-				await message.edit({ content: "**This deployment has started!**", embeds: [startedEmbed], components: rows }).catch(err => console.error("Message edit error:", err));
+				await message.edit({ content: "**This deployment has started!**", components: [] }).catch(err => console.error("Message edit error:", err));
 
 				deployment.started = true;
 				await deployment.save();
