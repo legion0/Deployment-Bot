050adaa551c26e7b9291190826d0ded3af4dbaef
srmullaney <srmullaney@gmail.com>
2024-11-03 15:10:35 -0500

    Ms secs all the same shit

--- src/utils/formatToGoogleCalendarDate.ts
+++ src/utils/formatToGoogleCalendarDate.ts
@@ -1,4 +1,4 @@
 export default function formatToGoogleCalendarDate(ms) {
-    const date = new Date(ms);
+    const date = new Date(ms / 1000);
     return date.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
 }
