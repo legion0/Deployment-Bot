8a97280f49603fd5bc37dbcd610006cf8fa46512
srmullaney <srmullaney@gmail.com>
2024-11-03 15:26:02 -0500

    U have to be shitting me

--- src/utils/formatToGoogleCalendarDate.ts
+++ src/utils/formatToGoogleCalendarDate.ts
@@ -1,9 +1,4 @@
 export default function formatToGoogleCalendarDate(ms: number) {
-    console.log(ms / 1000)
-    const date = new Date((ms / 1000));
-    console.log(date);
-    const dateString = date.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
-    console.log(dateString)
-
-    return dateString;
+    const date = new Date(Math.round(ms / 1000));
+    return date.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
 }
