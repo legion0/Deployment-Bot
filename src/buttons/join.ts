daabc09cd469f6c05505f7844461a49aee4b9da0
Paris <32800711+MacCaptKim@users.noreply.github.com>
2024-11-04 12:51:03 -0800

    blacklist

--- src/buttons/join.ts
+++ src/buttons/join.ts
@@ -3,6 +3,7 @@ import { client } from "../index.js";
 import Queue from "../tables/Queue.js";
 import { buildEmbed } from "../utils/configBuilders.js";
 import updateQueueMessages from "../utils/updateQueueMessage.js";
+import checkBlacklist from "../utils/checkBlacklist.js";
 
 export default new Button({
     id: "join",
@@ -10,6 +11,13 @@ export default new Button({
     permissions: [],
     requiredRoles: [],
     func: async function ({ interaction }) {
+        if (await checkBlacklist(interaction.user.id, interaction.guild)) {
+            const errorEmbed = buildEmbed({ preset: "error" })
+                .setDescription("You are blacklisted from joining queues");
+
+            return await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+
         await interaction.deferUpdate();
         const alreadyQueued = await Queue.findOne({ where: { user: interaction.user.id } });
 
