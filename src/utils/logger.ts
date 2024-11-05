bbb5f0e185ff630023bae86830d46df3ffa0f2ad
Paris <32800711+MacCaptKim@users.noreply.github.com>
2024-11-04 16:31:31 -0800

    Update logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,48 +3,48 @@ import config from "../config.js";
 
 export function log(text: string, context?: string) {
 	const date = new Date().toTimeString().split(/ +/)[0];
-	const contextStr = context ? colors.cyan(`[${context}]`) : '';
+	const contextStr = context ? colors.blue(`[${context}]`) : '';
 	console.log(colors.green(`[${date}]${contextStr}: ${text}`));
 }
 
 export function success(text: string, context?: string) {
 	const date = new Date().toTimeString().split(/ +/)[0];
-	const contextStr = context ? colors.cyan(`[${context}]`) : '';
-	console.log(colors.rainbow(`[${date}]${contextStr}:  ${text}`));
+	const contextStr = context ? colors.blue(`[${context}]`) : '';
+	console.log(colors.green(`[${date}]${contextStr}: ${text}`));
 }
 
 export function warn(text: string, context?: string) {
 	const date = new Date().toTimeString().split(/ +/)[0];
-	const contextStr = context ? colors.cyan(`[${context}]`) : '';
+	const contextStr = context ? colors.blue(`[${context}]`) : '';
 	console.log(colors.yellow(`[${date}]${contextStr}: ${text}`));
 }
 
 export function error(text: String | Error, context?: string) {
 	const date = new Date().toTimeString().split(/ +/)[0];
-	const contextStr = context ? colors.cyan(`[${context}]`) : '';
+	const contextStr = context ? colors.blue(`[${context}]`) : '';
 	
 	if (config.debugMode) {
 		if (text instanceof Error) {
-			console.log(colors.red.bold(`[${date}]${contextStr}: Error: ${text.message}`));
+			console.log(colors.red(`[${date}]${contextStr}: Error: ${text.message}`));
 			console.error(colors.red(text.stack));
 		} else {
-			console.log(colors.red.bold(`[${date}]${contextStr}: ${text}`));
+			console.log(colors.red(`[${date}]${contextStr}: ${text}`));
 		}
 	} else {
-		console.log(colors.red.bold(`[${date}]${contextStr}: ${text}`));
+		console.log(colors.red(`[${date}]${contextStr}: ${text}`));
 	}
 }
 
 export function debug(text: string, context?: string) {
 	if (config.debugMode) {
 		const date = new Date().toTimeString().split(/ +/)[0];
-		const contextStr = context ? colors.cyan(`[${context}]`) : '';
+		const contextStr = context ? colors.blue(`[${context}]`) : '';
 		console.log(colors.gray(`[${date}]${contextStr}: ${text}`));
 	}
 }
 
 export function action(text: string, context?: string) {
 	const date = new Date().toTimeString().split(/ +/)[0];
-	const contextStr = context ? colors.cyan(`[${context}]`) : '';
+	const contextStr = context ? colors.blue(`[${context}]`) : '';
 	console.log(colors.magenta(`[${date}]${contextStr}: ${text}`));
 }
