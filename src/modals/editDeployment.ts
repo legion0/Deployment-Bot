88725c2ab6afbb94272238db15b4f6e4771e0c63
srmullaney <srmullaney@gmail.com>
2024-11-08 14:31:42 -0500

    Fixed a small bug. Forgot to check if shit existed lol

--- src/modals/editDeployment.ts
+++ src/modals/editDeployment.ts
@@ -12,8 +12,8 @@ export default {
         if(!deployment) return;
 
         const startTimeInput = async () => {
+            if(!interaction.fields.getField("startTime")) return null;
             const startTime = interaction.fields.getTextInputValue("startTime");
-            if(!startTime) return null;
             let startDate:Date = null;
 
             try { startDate = await getStartTime(startTime, interaction); }
@@ -23,9 +23,9 @@ export default {
         }
 
         const details = {
-            title: interaction.fields.getTextInputValue("title"),
-            difficulty: interaction.fields.getTextInputValue("difficulty"),
-            description: interaction.fields.getTextInputValue("description"),
+            title: interaction.fields.getField("title") ? interaction.fields.getTextInputValue("title") : null,
+            difficulty: interaction.fields.getField("difficulty") ? interaction.fields.getTextInputValue("difficulty") : null,
+            description: interaction.fields.getField("description") ? interaction.fields.getTextInputValue("description") : null,
             startTime: startTimeInput(),
             endTime: this.startTime ? this.startTime.getTime() + 7200000 : null
         }
