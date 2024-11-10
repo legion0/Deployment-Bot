afbb4290a24f6f26acb0539368017aff8535921a
srmullaney <srmullaney@gmail.com>
2024-11-10 17:44:45 -0500

    idek

--- src/utils/interaction/checkBlacklist.ts
+++ src/utils/interaction/checkBlacklist.ts
@@ -10,6 +10,8 @@ export default async function checkBlacklist(interaction: ReplyableInteraction, 
     console.log("blacklist")
     const member = await interaction.guild.members.fetch(interaction.user.id);
     const br = blacklist.filter(roleId => member.roles.cache.has(roleId));
+    console.log(br)
+    console.log(br.length)
     if(br.length) return; // return false is they are not on the blacklist
     const description = `${br.length >= 3 ? `${br.slice(0, -1).join(', ')}, & ${br[br.length - 1]}` : `${br.join(" & ")}`} are not permitted to to use this interaction `;
     const errorEmbed = buildEmbed({ preset: "error" })
