0d8c2e3355f86d4cc76a03be01a50d83eb125c58
srmullaney <srmullaney@gmail.com>
2024-11-10 19:14:31 -0500

    It should work now

--- src/utils/interaction/checkBlacklist.ts
+++ src/utils/interaction/checkBlacklist.ts
@@ -5,7 +5,7 @@ export default async function checkBlacklist(interaction: ReplyableInteraction, 
     if(!blacklist.length) return false;
     const member = await interaction.guild.members.fetch(interaction.user.id);
     const br = blacklist.filter(roleId => member.roles.cache.has(roleId));
-    if(br.length) return false; // return false is they are not on the blacklist
+    if(!br.length) return false; // return false is they are not on the blacklist
     const description = `${br.length >= 3 ? `${br.slice(0, -1).join(', ')}, & ${br[br.length - 1]}` : `${br.join(" & ")}`} are not permitted to to use this interaction `;
     const errorEmbed = buildEmbed({ preset: "error" })
         .setTitle("Blacklisted Role!")
