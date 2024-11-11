50ec1a32ea5c9399368b77a425b1094966047314
srmullaney <srmullaney@gmail.com>
2024-11-10 19:30:17 -0500

    Updated blacklist to list plain text roles when showing error

--- src/utils/interaction/checkBlacklist.ts
+++ src/utils/interaction/checkBlacklist.ts
@@ -4,9 +4,10 @@ import ReplyableInteraction from "./ReplyableInteraction.js";
 export default async function checkBlacklist(interaction: ReplyableInteraction, blacklist: string[]):Promise<boolean> {
     if(!blacklist.length) return false;
     const member = await interaction.guild.members.fetch(interaction.user.id);
-    const br = blacklist.filter(roleId => member.roles.cache.has(roleId));
-    if(!br.length) return false; // return false is they are not on the blacklist
-    const description = `${br.length >= 3 ? `${br.slice(0, -1).join(', ')}, & ${br[br.length - 1]}` : `${br.join(" & ")}`} are not permitted to to use this interaction `;
+    const blacklistedRoles = blacklist.filter(roleId => member.roles.cache.has(roleId));
+    if(!blacklistedRoles.length) return false; // return false is they are not on the blacklist
+    const blacklistedRolesNames = blacklistedRoles.map(async (roleId) => await interaction.guild.roles.fetch(roleId))
+    const description = `${blacklistedRolesNames.length >= 3 ? `${blacklistedRolesNames.slice(0, -1).join("'s , ")}'s, & ${blacklistedRolesNames[blacklistedRolesNames.length - 1]}` : `${blacklistedRolesNames.join("'s & ")}'s`} are not permitted to to use this interaction.`;
     const errorEmbed = buildEmbed({ preset: "error" })
         .setTitle("Blacklisted Role!")
         .setDescription(description);
